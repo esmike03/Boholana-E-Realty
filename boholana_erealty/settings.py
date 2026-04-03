e4347002fd4c8c8b999dfc332109e824924a38b4
Earl Mike Sarabia <sarabiaearlmike14@gmail.com>
2026-04-03 23:14:44 +0800

    Final

--- boholana_erealty/settings.py
+++ boholana_erealty/settings.py
@@ -173,7 +173,10 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'theme' / 'static',  # 👈 ADD THIS
+]
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
